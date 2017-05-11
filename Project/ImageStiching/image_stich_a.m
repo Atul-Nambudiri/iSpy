2f6f4683a967cfa607233e3340827ff00fcf0a85
nambudi2 <nambudi2@illinois.edu>
2017-05-11 01:27:30 -0500

    Update

--- Project/ImageStiching/image_stich_a.m
+++ Project/ImageStiching/image_stich_a.m
@@ -1,83 +1,96 @@
-function [ims] = image_stich_a(images)
+function [panorama] = image_stich_a(images)
     I3 = zeros(3);
     for i = 1:3
         I3(i, i) = 1;
     end
     
     for i = 1:length(images)
-        trans_matrices(i) = maketform('projective', I3);
+        trans_matrices(i) = projective2d(I3);
     end
     
     for i = 2:length(images)
         T = getTForm(images{i}, images{i-1});
-        trans_matrices(i).tdata.T = trans_matrices(i-1).tdata.T * T.T;
+        trans_matrices(i) = trans_matrices(i-1).T * T.T;
     end
     
     XPos = zeros(length(images), 2);
+    
+    [x, y] = size(images{i});
 
-    for i = 1:length(images) 
-        tempT = trans_matrices(i);
-        [~,xcoord,ycoord] = imtransform(images{i}, tempT);
-        XPos(i, :) = xcoord;   
+    imageSize = size(images{i});  % all the images are the same size
+
+    % Compute the output limits  for each transform
+    for i = 1:length(trans_matrices)           
+        [xlim(i,:), ylim(i,:)] = outputLimits(trans_matrices(i), [1 imageSize(2)], [1 imageSize(1)]);    
     end
-    
-    [~, index] = sort(mean(XPos, 2));
-    center = floor((length(trans_matrices)+1)/2);
 
-    center_idx = index(center);
+    %%
+    % Next, compute the average X limits for each transforms and find the image
+    % that is in the center. Only the X limits are used here because the scene
+    % is known to be horizontal. If another set of images are used, both the X
+    % and Y limits may need to be used to find the center image.
 
-%     for i = 1:length(images)    
-%         trans_matrices(i).tdata.T = inv(trans_matrices(center_idx).tdata.T) * trans_matrices(i).tdata.T;
-%     end
-    
-%     Tinv = inv(trans_matrices(center_idx).tdata.T);
+    avgXLim = mean(xlim, 2);
+
+    [~, idx] = sort(avgXLim);
+
+    centerIdx = floor((numel(trans_matrices)+1)/2);
+
+    centerImageIdx = idx(centerIdx);
+
+    %%
+    % Finally, apply the center image's inverse transform to all the others.
+
+    Tinv = invert(trans_matrices(centerImageIdx));
 
     for i = 1:numel(trans_matrices)    
-        trans_matrices(i).tdata.T = inv(trans_matrices(center_idx).tdata.T) * trans_matrices(i).tdata.T;
+        trans_matrices(i).T = Tinv.T * trans_matrices(i).T;
     end
-    
-    xMin = 1;
-    yMin = 1;
-    
-    yMax = 1;
-    xMax = 1;
 
-    for i = 1:length(images) 
-        [~,xcoord,ycoord] = imtransform(images{i},trans_matrices(i));
-        xMin = min([xMin, xcoord(1)]);
-        yMin = min([yMin, ycoord(1)]);
-        xMax = max([xMax, xcoord(2)]);
-        yMax = max([yMax, ycoord(2)]);
+    %% Step 3 - Initialize the Panorama
+    % Now, create an initial, empty, panorama into which all the images are
+    % mapped. 
+    % 
+    % Use the |outputLimits| method to compute the minimum and maximum output
+    % limits over all transformations. These values are used to automatically
+    % compute the size of the panorama.
+
+    for i = 1:length(trans_matrices)           
+        [xlim(i,:), ylim(i,:)] = outputLimits(trans_matrices(i), [1 imageSize(2)], [1 imageSize(1)]);
     end
-    
-%     panX  = round(xMax - xMin);
-%     panY = round(yMax - yMin);
+
+    % Find the minimum and maximum output limits 
+    xMin = min([1; xlim(:)]);
+    xMax = max([imageSize(2); xlim(:)]);
+
+    yMin = min([1; ylim(:)]);
+    yMax = max([imageSize(1); ylim(:)]);
+
+    % Width and height of panorama.
+    width  = round(xMax - xMin);
+    height = round(yMax - yMin);
+
+    % Initialize the "empty" panorama.
+    panorama = zeros([height width 3], 'like', images{1});
 
     %% Step 4 - Create the Panorama
     % Use |imwarp| to map images into the panorama and use
     % |vision.AlphaBlender| to overlay the images together.
-    
-    %panorama = zeros([panX panY 3], 'like', images{1});
-    panorama = zeros([round(yMax - yMin) round(xMax - xMin) 3], 'like', images{1});
 
     blender = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');  
 
     % Create a 2-D spatial reference object defining the size of the panorama.
     xLimits = [xMin xMax];
     yLimits = [yMin yMax];
-    [p_rows, p_cols, ~] = size(panorama);
-    panoramaView = imref2d([p_rows p_cols], xLimits, yLimits);
+    panoramaView = imref2d([height width], xLimits, yLimits);
 
     % Create the panorama.
-    for i = 1:length(images)
+    for i = 1:length(images);
 
         I = images{i};
-        
-        curr_projection = projective2d(I3);
-        curr_projection.T = trans_matrices(i).tdata.T;
 
         % Transform I into the panorama.
-        warpedImage = imwarp(I, curr_projection, 'OutputView', panoramaView);
+        warpedImage = imwarp(I, trans_matrices(i), 'OutputView', panoramaView);
 
         % Overlay the warpedImage onto the panorama.
         panorama = step(blender, panorama, warpedImage, warpedImage(:,:,1));
@@ -85,6 +98,5 @@ function [ims] = image_stich_a(images)
 
     figure
     imshow(panorama)
-    
     
 end
--- Project/ImageStiching/runThis_a.m
+++ Project/ImageStiching/runThis_a.m
@@ -1,21 +1,21 @@
 % img_path = strcat('images/test_', num2str(img_idx), '.jpg');
-im1 = imresize(imread('images/rso1.jpg'), .3);
-im2 = imresize(imread('images/rso2.jpg'), .3);
-im3 = imresize(imread('images/rso3.jpg'), .3);
-im4 = imresize(imread('images/rso4.jpg'), .3);
-im5 = imresize(imread('images/rso5.jpg'), .3);
-im6 = imresize(imread('images/rso6.jpg'), .3);
+im1 = imread('images/1.jpg');
+im2 = imread('images/2.jpg');
+im3 = imread('images/3.jpg');
+% im4 = imresize(imread('images/rso4.jpg'), .3);
+% im5 = imresize(imread('images/rso5.jpg'), .3);
+% im6 = imresize(imread('images/rso6.jpg'), .3);
 
-images = cell(6,1);
+images = cell(2,1);
 
 images{1} = im1;
 images{2} = im2;
 images{3} = im3;
-images{4} = im4;
-images{5} = im5;
-images{6} = im6;
+% images{4} = im4;
+% images{5} = im5;
+% images{6} = im6;
 
 pan1 = image_stich_a(images);
 
-% figure(1)
-% imshow(pan1);
+figure(1)
+imshow(pan1);
