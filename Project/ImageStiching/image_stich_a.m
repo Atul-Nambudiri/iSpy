8ebf20a7e34f632647d3a8fa5b8fed5ba8f1ffec
nambudi2 <nambudi2@illinois.edu>
2017-05-12 02:23:22 -0500

    Updated

--- Project/ImageStiching/image_stich_a.m
+++ Project/ImageStiching/image_stich_a.m
@@ -14,89 +14,45 @@ function [panorama] = image_stich_a(images)
     end
     
     XPos = zeros(length(images), 2);
+    [x, y, z] = size(images{i});
+
+    for i = 1:length(trans_matrices)           
+        [XPos(i, :), ~] = outputLimits(trans_matrices(i), [1 y], [1 x]);    
+    end
+
+    [~, ind] = sort(mean(XPos, 2));
+    center = floor((length(trans_matrices)+1)/2);
+    center_idx = ind(center);
+
+    for i = 1:length(trans_matrices)    
+        trans_matrices(i).T = inv(trans_matrices(center_idx).T) * trans_matrices(i).T;
+    end
+
+    for i = 1:length(trans_matrices)           
+        [xPos(i,:), ~] = outputLimits(trans_matrices(i), [1 y], [1 x]);
+    end
+
+    xMin = 1;
+    yMin = 1;
     
-    [x, y] = size(images{i});
-
-    imageSize = size(images{i});  % all the images are the same size
-
-    % Compute the output limits  for each transform
-    for i = 1:length(trans_matrices)           
-        [xlim(i,:), ylim(i,:)] = outputLimits(trans_matrices(i), [1 imageSize(2)], [1 imageSize(1)]);    
+    yMax = 1;
+    xMax = 1;
+    
+    for i = 1:length(images) 
+        [xcoord, ycoord] = outputLimits(trans_matrices(i), [1 y], [1 x]);
+        xMin = min([xMin, xcoord(1)]);
+        yMin = min([yMin, ycoord(1)]);
+        xMax = max([xMax, xcoord(2)]);
+        yMax = max([yMax, ycoord(2)]);
     end
-
-    %%
-    % Next, compute the average X limits for each transforms and find the image
-    % that is in the center. Only the X limits are used here because the scene
-    % is known to be horizontal. If another set of images are used, both the X
-    % and Y limits may need to be used to find the center image.
-
-    avgXLim = mean(xlim, 2);
-
-    [~, idx] = sort(avgXLim);
-
-    centerIdx = floor((numel(trans_matrices)+1)/2);
-
-    centerImageIdx = idx(centerIdx);
-
-    %%
-    % Finally, apply the center image's inverse transform to all the others.
-
-    Tinv = invert(trans_matrices(centerImageIdx));
-
-    for i = 1:numel(trans_matrices)    
-        trans_matrices(i).T = Tinv.T * trans_matrices(i).T;
-    end
-
-    %% Step 3 - Initialize the Panorama
-    % Now, create an initial, empty, panorama into which all the images are
-    % mapped. 
-    % 
-    % Use the |outputLimits| method to compute the minimum and maximum output
-    % limits over all transformations. These values are used to automatically
-    % compute the size of the panorama.
-
-    for i = 1:length(trans_matrices)           
-        [xlim(i,:), ylim(i,:)] = outputLimits(trans_matrices(i), [1 imageSize(2)], [1 imageSize(1)]);
-    end
-
-    % Find the minimum and maximum output limits 
-    xMin = min([1; xlim(:)]);
-    xMax = max([imageSize(2); xlim(:)]);
-
-    yMin = min([1; ylim(:)]);
-    yMax = max([imageSize(1); ylim(:)]);
-
-    % Width and height of panorama.
-    width  = round(xMax - xMin);
-    height = round(yMax - yMin);
-
-    % Initialize the "empty" panorama.
-    panorama = zeros([height width 3], 'like', images{1});
-
-    %% Step 4 - Create the Panorama
-    % Use |imwarp| to map images into the panorama and use
-    % |vision.AlphaBlender| to overlay the images together.
-
-    blender = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');  
-
-    % Create a 2-D spatial reference object defining the size of the panorama.
-    xLimits = [xMin xMax];
-    yLimits = [yMin yMax];
-    panoramaView = imref2d([height width], xLimits, yLimits);
-
-    % Create the panorama.
-    for i = 1:length(images);
-
-        I = images{i};
-
-        % Transform I into the panorama.
-        warpedImage = imwarp(I, trans_matrices(i), 'OutputView', panoramaView);
-
-        % Overlay the warpedImage onto the panorama.
+    
+    blender = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');
+    panorama = zeros([round(yMax - yMin) round(xMax - xMin) 3], 'like', images{1});
+    pan_ref = imref2d([round(yMax - yMin) round(xMax - xMin)], [xMin xMax], [yMin yMax]);
+    
+    for t = 1:length(images);
+        [warpedImage, RA] = imwarp(images{t}, trans_matrices(t), 'OutputView', pan_ref);
         panorama = step(blender, panorama, warpedImage, warpedImage(:,:,1));
     end
-
-    figure
-    imshow(panorama)
     
 end
--- Project/ImageStiching/runThis_a.m
+++ Project/ImageStiching/runThis_a.m
@@ -3,8 +3,8 @@
 % im2 = imread('images/closeup2.jpg');
 % im3 = imread('images/closeup3.jpg');
 
-im1 = imread('images/eceb1.jpg');
-im2 = imread('images/eceb2.jpg');
+im1 = imresize(imread('images/eceb1.jpg'), .3);
+im2 = imresize(imread('images/eceb2.jpg'), .3);
 
 % im4 = imresize(imread('images/rso4.jpg'), .3);
 % im5 = imresize(imread('images/rso5.jpg'), .3);
@@ -19,24 +19,26 @@ images{2} = im2;
 % images{5} = im5;
 % images{6} = im6;
 
+[x, y, z] = size(im1);
+
 HSV1 = rgb2hsv(im1);
 HSV2 = rgb2hsv(im2);
 v_avg1 = 0;
 v_avg2 = 0;
-for j = 1:4032
-    for i = 1:3024
+for j = 1:y
+    for i = 1:x
         v_avg1 = v_avg1 + HSV1(i, j, 3);
         v_avg2 = v_avg2 + HSV2(i, j, 3);
     end
 end
 
-v_avg1 = v_avg1/(4032*3024);
-v_avg2 = v_avg2/(4032*3024);
+v_avg1 = v_avg1/(x*y);
+v_avg2 = v_avg2/(x*y);
 
 v_add = v_avg1 - v_avg2;
 
-for j = 1:4032
-    for i = 1:3024
+for j = 1:y
+    for i = 1:x
         HSV2(i, j, 3) = HSV2(i, j, 3) + v_add;
     end
 end
@@ -46,5 +48,5 @@ images{2} = im2;
 
 pan1 = image_stich_a(images);
 
-figure(1)
+figure(5)
 imshow(pan1);
